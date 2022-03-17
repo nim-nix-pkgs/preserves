1ed1f98e66203c3bcb9b976e82733745668ce442
hugosenari <hugosenari@gmail.com>
2022-03-17 18:27:57 -0300

    chore: re-index ../nimpkgs/p/preserves/v0_2_0

--- v0_2_0/flake.nix
+++ v0_2_0/flake.nix
@@ -11,7 +11,7 @@
   inputs.src-preserves-v0_2_0.owner = "~ehmry";
   inputs.src-preserves-v0_2_0.ref   = "refs/tags/v0.2.0";
   inputs.src-preserves-v0_2_0.repo  = "preserves-nim";
-  inputs.src-preserves-v0_2_0.type  = "other";
+  inputs.src-preserves-v0_2_0.type  = "sourcehut";
   
   inputs."bigints".owner = "nim-nix-pkgs";
   inputs."bigints".ref   = "master";
