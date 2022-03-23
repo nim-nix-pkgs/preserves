059958cf281b8dda0c00d4f77c756b0bbcc8ddff
hugosenari <hugosenari@gmail.com>
2022-03-22 21:27:44 -0300

    feat(nimpkgs): remove lock

--- v0_2_0/flake.nix
+++ v0_2_0/flake.nix
@@ -8,7 +8,7 @@
   inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
   
   inputs.src-preserves-v0_2_0.flake = false;
-  inputs.src-preserves-v0_2_0.ref   = "refs/tags/v0.2.0";
+  inputs.src-preserves-v0_2_0.ref   = "v0.2.0";
   inputs.src-preserves-v0_2_0.owner = "~ehmry";
   inputs.src-preserves-v0_2_0.repo  = "preserves-nim";
   inputs.src-preserves-v0_2_0.type  = "sourcehut";
